2e7787bab3bc83339ae9786226a2cb2b47078aa9
Jonathan Bergqvist <jonathan@seednode.io>
2023-05-01 13:50:53 +0200

    Fix sequence length issues

--- src/model/transformer.py
+++ src/model/transformer.py
@@ -7,20 +7,26 @@ from .decoder import Decoder
 
 
 class Transformer(nn.Module):
+    def __init__(self, block_size):
+        super().__init__()
+
+        self.block_size = block_size
+        self.register_buffer("tri", torch.tril(torch.ones(block_size, block_size)))
+
     def create_lookahead_mask(self, tgt_seq_len):
-        ones = torch.ones(tgt_seq_len, tgt_seq_len, dtype=torch.uint8)
+        return self.tri[:tgt_seq_len, :tgt_seq_len].unsqueeze(0)
 
-        return torch.tril(ones, diagonal=0).unsqueeze(0)
+    def generate(self, inp_seq, eos_token_id=2, max_output_len=100):
+        B, T = inp_seq.shape
+        device = inp_seq.device
 
-    def generate(self, input, eos_token_id=2, max_output_len=100):
-        B, T = input.shape
-        seq = input
+        seq = inp_seq
 
         # Create mask for checking which generated sequences have encountered end-of-sequence (EOS) tokens.
-        eos_mask = torch.zeros((B, 1), dtype=torch.bool)
+        eos_mask = torch.zeros((B, 1), dtype=torch.bool, device=device)
 
         for _ in range(max_output_len):
-            out = self.forward(seq)
+            out = self.forward(seq[..., -self.block_size :])  # Truncate input sequence.
             probs = F.softmax(out[:, -1, :], dim=1)
             next_tokens = torch.multinomial(probs, num_samples=1)
 
@@ -29,7 +35,9 @@ class Transformer(nn.Module):
             eos_mask = eos_mask | has_eos
 
             # Set the next tokens to EOS tokens for sequences that have already encountered an EOS.
-            next_tokens = torch.where(eos_mask, torch.tensor(eos_token_id), next_tokens)
+            next_tokens = torch.where(
+                eos_mask, torch.tensor(eos_token_id, device=device), next_tokens
+            )
 
             # Append the next tokens to the generated sequences.
             seq = torch.cat((seq, next_tokens), dim=-1)
@@ -44,8 +52,10 @@ class Transformer(nn.Module):
 class TransformerEncoderDecoder(Transformer):
     """Transformer encoder-decoder, using cross-attention with encoded input sequence when decoding output."""
 
-    def __init__(self, vocab_size, max_seq_len, hidden_size, ff_hidden_size):
-        super().__init__()
+    def __init__(
+        self, vocab_size, max_seq_len, hidden_size, ff_hidden_size, block_size
+    ):
+        super().__init__(block_size)
 
         self.encoder = Encoder(vocab_size, max_seq_len, hidden_size, ff_hidden_size)
         self.decoder = Decoder(vocab_size, max_seq_len, hidden_size, ff_hidden_size)
@@ -62,8 +72,10 @@ class TransformerEncoderDecoder(Transformer):
 class TransformerDecoder(Transformer):
     """Transformer decoder, using auto-regressive decoder blocks for language modeling."""
 
-    def __init__(self, vocab_size, max_seq_len, hidden_size, ff_hidden_size):
-        super().__init__()
+    def __init__(
+        self, vocab_size, max_seq_len, hidden_size, ff_hidden_size, block_size
+    ):
+        super().__init__(block_size)
 
         self.decoder = Decoder(vocab_size, max_seq_len, hidden_size, ff_hidden_size)
 
@@ -73,8 +85,3 @@ class TransformerDecoder(Transformer):
         out = self.decoder(tgt, None, tgt_mask)
 
         return out
-
-    def create_lookahead_mask(self, tgt_seq_len):
-        ones = torch.ones(tgt_seq_len, tgt_seq_len, dtype=torch.uint8)
-
-        return torch.tril(ones, diagonal=0).unsqueeze(0)
