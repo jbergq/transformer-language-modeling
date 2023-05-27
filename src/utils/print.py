32755d23f2c8a889cd064610e76b8aaaeaa7a5cb
Jonathan Bergqvist <jonathan@seednode.io>
2023-05-27 13:39:22 +0200

    Refactor training loop from epoch-based to step-based

--- src/utils/print.py
+++ src/utils/print.py
@@ -1,23 +1,19 @@
-def iter_print(epoch, iter, train_loss):
+def iter_print(iter, train_loss, newline_interval=50):
     l_string = "-" * 45
-    f_str = "{: <10} {: <10} {: <10.5}"
+    f_str = "{: <10} {: <10.5}"
 
     if iter % 500 == 0:
         print(l_string)
-        print(f_str.format("Epoch", "Iter", "Loss"))
+        print(f_str.format("Iter", "Train loss"))
         print(l_string)
-    print(f_str.format(epoch, iter, train_loss), end="\r" if iter % 50 else "\n")
+    print(f_str.format(iter, train_loss), end="\r" if iter % newline_interval else "\n")
 
 
-def epoch_print(epoch, val_losses):
+def evaluation_print(losses):
     e_string = "=" * 45
 
-    num_val = len(val_losses)
-    val_sum = sum(val_losses)
-    val_mean = val_sum / num_val
-
     print("\n\n" + e_string)
-    print("Epoch {epoch} done!".format(epoch=epoch))
-    print("Total validation loss: {val_sum:.3f}".format(val_sum=val_sum))
-    print("Average validation loss: {val_mean:.3f}".format(val_mean=val_mean))
+    print("Evaluation done!")
+    print("Mean train loss: {mean_loss:.3f}".format(mean_loss=losses["train"]))
+    print("Mean validation loss: {mean_loss:.3f}".format(mean_loss=losses["val"]))
     print(e_string + "\n")
