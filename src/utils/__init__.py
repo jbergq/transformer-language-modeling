48c7a2e9add35ab1dc6fbf587111d06324a9981a
Jonathan Bergqvist <jonathan@seednode.io>
2023-11-26 22:38:19 +0100

    Move all code into train.ipynb

--- src/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .config import *
-from .easy_dict import *
-from .print import *
--- src/utils/params.py
+++
@@ -1,8 +0,0 @@
-def count_model_params(model):
-    total_params = 0
-    for params in list(model.parameters()):
-        num = 1
-        for size in list(params.size()):
-            num = num * size
-        total_params += num
-    return total_params
