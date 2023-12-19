d18aeb745ffc4cacf80c48036f6e2845dafd9fa4
Jonathan Bergqvist <jonathan@seednode.io>
2023-12-19 20:27:21 +0100

    Updates to train.ipynb

---
+++ utils.py
@@ -0,0 +1,41 @@
+def count_model_params(model):
+    total_params = 0
+    for params in list(model.parameters()):
+        num = 1
+        for size in list(params.size()):
+            num = num * size
+        total_params += num
+    return total_params
+
+
+def evaluation_print(losses):
+    e_string = "=" * 45
+
+    print("\n\n" + e_string)
+    print("Evaluation done!")
+    print("Mean train loss: {mean_loss:.3f}".format(mean_loss=losses["train"]))
+    print("Mean validation loss: {mean_loss:.3f}".format(mean_loss=losses["val"]))
+    print(e_string + "\n")
+
+
+def iter_print(iter, train_loss, newline_interval=50):
+    l_string = "-" * 45
+    f_str = "{: <10} {: <10.5}"
+
+    if iter % 500 == 0:
+        print(l_string)
+        print(f_str.format("Iter", "Train loss"))
+        print(l_string)
+    print(f_str.format(iter, train_loss), end="\r" if iter % newline_interval else "\n")
+
+
+def train_start_print(model):
+    e_string = "=" * 45
+
+    num_params = count_model_params(model)
+    num_params_m = num_params / 1e6
+
+    print("\n" + e_string)
+    print("Starting training!")
+    print("Num model params: {num_params_m:.3f}M".format(num_params_m=num_params_m))
+    print(e_string + "\n")
